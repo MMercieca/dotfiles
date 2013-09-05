a041065ada3bf1df0759573cd49a2fa34b0c68a5
Mike Simons <simonsm2@gmail.com>
2013-09-05 14:54:52 -0400

    Adding issues alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,3 +10,4 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias hb="hub browse"
+alias hi="hub browse -- issues"
